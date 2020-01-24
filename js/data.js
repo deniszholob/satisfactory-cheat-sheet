0a9106711962ef94ad6c7b920c443ef3b3de9785
Denis Zholob <dddgamer3x@gmail.com>
2020-01-24 04:31:41 -0500

    Add more ratios

--- js/data.js
+++ js/data.js
@@ -3,6 +3,7 @@ export const data_common_ratios = {
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=iron-ingot:f:2",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Iron Ingot",
@@ -19,6 +20,7 @@ export const data_common_ratios = {
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=copper-ingot:f:2",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Copper Ingot",
@@ -35,6 +37,7 @@ export const data_common_ratios = {
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=concrete:f:4",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Concrete",
@@ -51,6 +54,7 @@ export const data_common_ratios = {
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=quickwire:f:4",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Quickwire",
@@ -69,11 +73,167 @@ export const data_common_ratios = {
         }
       ]
     },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=steel-ingot:f:4",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Steel Ingot",
+          "machine": "Smelter",
+          "count": 4
+        },
+        {
+          "product": "Iron Ore",
+          "machine": "Miner MK1",
+          "count": 3
+        },
+        {
+          "product": "Coal",
+          "machine": "Miner MK1",
+          "count": 3
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=silica:f:1,quartz-crystal:f:1",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Silica",
+          "machine": "Constructor",
+          "count": 1
+        },
+        {
+          "product": "Quartz Crystal",
+          "machine": "Constructor",
+          "count": 1
+        },
+        {
+          "product": "Raw Quartz",
+          "machine": "Miner MK1",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=black-powder:f:4",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Black Powder",
+          "machine": "Constructor",
+          "count": 1
+        },
+        {
+          "product": "Coal",
+          "machine": "Miner MK1",
+          "count": 0.5
+        },
+        {
+          "product": "Sulfur",
+          "machine": "Miner MK1",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=compacted-coal:f:2",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Compacted Coal",
+          "machine": "Constructor",
+          "count": 1
+        },
+        {
+          "product": "Coal",
+          "machine": "Miner MK1",
+          "count": 1
+        },
+        {
+          "product": "Sulfur",
+          "machine": "Miner MK1",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=fuel:f:2",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Fuel",
+          "machine": "Oil Refinery",
+          "count": 2
+        },
+        {
+          "product": "Crude Oil",
+          "machine": "Oil Pump",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=rubber:f:4",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Rubber",
+          "machine": "Oil Refinery",
+          "count": 4
+        },
+        {
+          "product": "Crude Oil",
+          "machine": "Oil Pump",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=aluminum-ingot:f:4&miners=raw-quartz:miner-mk2:1,bauxite:miner-mk2:1",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Aluminum Ingot",
+          "machine": "Smelter",
+          "count": 4
+        },
+        {
+          "product": "Bauxite",
+          "machine": "Miner MK2",
+          "count": 3.5
+        },
+        {
+          "product": "Silica",
+          "machine": "Constructor",
+          "count": 8
+        },
+        {
+          "product": "Raw Quartz",
+          "machine": "Miner MK2",
+          "count": 2
+        }
+      ]
+    },
   ],
+
+
+
+
+
+
   "Ratios_Construction": [
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=cable:f:3",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Cable",
@@ -95,6 +255,7 @@ export const data_common_ratios = {
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=iron-plate:f:1",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Iron Plate",
@@ -111,6 +272,7 @@ export const data_common_ratios = {
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=screw:f:2",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Screw",
@@ -130,8 +292,9 @@ export const data_common_ratios = {
       ]
     },
     {
-      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=screw:f:2",
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=reinforced-iron-plate:f:3",
       "name": "",
+      "isAllInclusive": true,
       "ratio": [
         {
           "product": "Reinforced Iron Plate",
@@ -153,21 +316,193 @@ export const data_common_ratios = {
     {
       "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=modular-frame:f:5",
       "name": "",
+      "isAllInclusive": false,
       "ratio": [
+        {
+          "product": "Modular Frame",
+          "machine": "Constructor",
+          "count": 5
+        },
         {
           "product": "Reinforced Iron Plate",
           "machine": "Assembler",
-          "count": 3
+          "count": 12
         },
         {
-          "product": "Iron Plate",
+          "product": "Iron Rod",
           "machine": "Constructor",
-          "count": 4
+          "count": 8
+        },
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=rotor:f:15",
+      "name": "",
+      "isAllInclusive": false,
+      "ratio": [
+        {
+          "product": "Rotor",
+          "machine": "Assembler",
+          "count": 15
+        },
+        {
+          "product": "Iron Rod",
+          "machine": "Constructor",
+          "count": 18
         },
         {
           "product": "Screw",
           "machine": "Constructor",
+          "count": 22
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=steel-beam:f:1",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Steel Beam",
+          "machine": "Constructor",
+          "count": 1
+        },
+        {
+          "product": "Steel Ingot",
+          "machine": "Foundry",
+          "count": 1
+        },
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=steel-pipe:f:2",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Steel Pipe",
+          "machine": "Constructor",
+          "count": 2
+        },
+        {
+          "product": "Steel Ingot",
+          "machine": "Foundry",
+          "count": 1
+        },
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=encased-industrial-beam:f:15",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Encased Industrial Beam",
+          "machine": "Assembler",
+          "count": 15
+        },
+        {
+          "product": "Steel Beam",
+          "machine": "Constructor",
+          "count": 24
+        },
+        {
+          "product": "Concrete",
+          "machine": "Constructor",
+          "count": 20
+        },
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=stator:f:15",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Stator",
+          "machine": "Assembler",
+          "count": 15
+        },
+        {
+          "product": "Steel Pipe",
+          "machine": "Constructor",
+          "count": 18
+        },
+        {
+          "product": "Wire",
+          "machine": "Constructor",
+          "count": 20
+        },
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=motor:f:3",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Motor",
+          "machine": "Assembler",
+          "count": 3
+        },
+        {
+          "product": "Rotor",
+          "machine": "Assembler",
+          "count": 5
+        },
+        {
+          "product": "Stator",
+          "machine": "Assembler",
+          "count": 5
+        },
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=circuit-board:f:3",
+      "name": "",
+      "isAllInclusive": true,
+      "ratio": [
+        {
+          "product": "Circuit Board",
+          "machine": "Assembler",
+          "count": 3
+        },
+        {
+          "product": "Wire",
+          "machine": "Constructor",
           "count": 4
+        },
+        {
+          "product": "Plastic",
+          "machine": "Oil Refinery",
+          "count": 4
+        },
+        {
+          "product": "Crude Oil",
+          "machine": "Oil Pump",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "src": "https://kirkmcdonald.github.io/satisfactory-calculator/calc.html#tab=totals&items=cartridge:f:1",
+      "name": "",
+      "isAllInclusive": false,
+      "ratio": [
+        {
+          "product": "Cartridge",
+          "machine": "Assembler",
+          "count": 1
+        },
+        {
+          "product": "Nobelisk",
+          "machine": "Manufacturer",
+          "count": 3
+        },
+        {
+          "product": "Black Powder",
+          "machine": "Constructor",
+          "count": 2
         }
       ]
     },
